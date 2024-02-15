8919898e6c3addace12a88988b9ada80f5212e0d
Viranchi Bhure <87461398+ViranchiB@users.noreply.github.com>
2024-02-15 11:59:36 +0000

    Data Types

---
+++ test.js
@@ -0,0 +1,10 @@
+// Data types
+// number => 2 to power 53
+// string => ""
+// boolean => true / false
+// null => standalone (empty)
+// undefinrd => not assignrd
+// symbol => unique
+
+// object
+
